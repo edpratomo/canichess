6a2c15aff94602a8beb9c69f7a0d7b0d517c3e73
Edwin Pratomo <edpratomo@cpan.org>
2023-05-02 20:30:02 +0700

    added /admin route

--- config/routes.rb
+++ config/routes.rb
@@ -44,5 +44,6 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  
+
+  get '/admin', to: redirect('/admin/tournaments')  
 end

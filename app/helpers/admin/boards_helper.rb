2f035da951bc198d111020422ed78785cdb88a08
Edwin Pratomo <edpratomo@cpan.org>
2023-05-03 20:16:26 +0700

    show players' points on pairings page

--- app/helpers/admin/boards_helper.rb
+++ app/helpers/admin/boards_helper.rb
@@ -1,2 +1,7 @@
 module Admin::BoardsHelper
+  def points(tournament_player, round)
+    return '' if round < 2
+    return '' unless tournament_player
+    "(#{tournament_player.standings.find_by(round: round - 1).points})"
+  end
 end

3b07ce583adf37baab68a776202dec15def3e306
Edwin Pratomo <edpratomo@cpan.org>
2023-05-18 02:23:25 +0700

    fixed boards helper

--- app/helpers/admin/boards_helper.rb
+++ app/helpers/admin/boards_helper.rb
@@ -6,22 +6,34 @@ module Admin::BoardsHelper
   end
 
   def disable_finalize_round
+    raw('<td>') +
     button_tag("Finalize this round", id: "finalize_enabled", class: "btn btn-block btn-primary btn-lg", style: "display: none", data: { confirm: "Are you sure?" }) +
-    button_tag("Finalize this round", id: "finalize_disabled", class: "btn btn-block btn-secondary btn-lg disabled", disabled: true)
+    raw('</td><td>') +
+    button_tag("Finalize this round", id: "finalize_disabled", class: "btn btn-block btn-secondary btn-lg disabled", disabled: true) +
+    raw('</td>')
   end
 
   def enable_finalize_round
+    raw('<td>') +
     button_tag("Finalize this round", id: "finalize_enabled", class: "btn btn-block btn-primary btn-lg", data: { confirm: "Are you sure?" }) +
-    button_tag("Finalize this round", id: "finalize_disabled", class: "btn btn-block btn-secondary btn-lg disabled", disabled: true, style: "display: none")
+    raw('</td><td>') +
+    button_tag("Finalize this round", id: "finalize_disabled", class: "btn btn-block btn-secondary btn-lg disabled", disabled: true, style: "display: none") +
+    raw('</td>')
   end
 
   def disable_delete_round
+    raw('<td>') +
     button_tag("Delete this round", class: "btn btn-block btn-danger btn-lg", style: "display: none", data: { confirm: "Are you sure?" }) +
-    button_tag("Delete this round", class: "btn btn-block btn-secondary btn-lg disabled", disabled: true, data: { confirm: "Are you sure?" })
+    raw('</td><td>') +
+    button_tag("Delete this round", class: "btn btn-block btn-secondary btn-lg disabled", disabled: true, data: { confirm: "Are you sure?" }) +
+    raw('</td>')
   end
 
   def enable_delete_round
+    raw('<td>') +
     button_tag("Delete this round", id: "delete_enabled", class: "btn btn-block btn-danger btn-lg", data: { confirm: "Are you sure?" }) +
-    button_tag("Delete this round", id: "delete_disabled", class: "btn btn-block btn-secondary btn-lg disabled", style: "display: none", disabled: true, data: { confirm: "Are you sure?" })
+    raw('</td><td>') +
+    button_tag("Delete this round", id: "delete_disabled", class: "btn btn-block btn-secondary btn-lg disabled", style: "display: none", disabled: true, data: { confirm: "Are you sure?" }) +
+    raw('</td>')
   end
 end

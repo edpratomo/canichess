24043aeb8e01ef85f1ac6aa98ad951dd6b030c60
Edwin Pratomo <edpratomo@cpan.org>
2024-04-07 22:19:32 +0700

    uses Rack hijacking API

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,33 +5,39 @@ class EventsController < ApplicationController
   before_action :set_tournament
   before_action :set_round, only: %i[ pairings ]
 
+  # Rack hijacking API (partial)
   def pairings
-    response.headers['Content-Type'] = 'text/event-stream'
-    response.headers['Last-Modified'] = Time.now.httpdate
-    sse = SSE.new(response.stream, event: "message")
-
-    # long polling
-    begin
-      loop do
-        data = @tournament.get_results(@round)
-        unless data.empty?
-          sse.write(JSON.pretty_generate(data))
-        end
-        sleep 3
+    response.headers["Content-Type"] = "text/event-stream"
+    response.headers["rack.hijack"] = proc do |stream|
+      Thread.new do
+        perform_task(stream)
       end
-    rescue ActionController::Live::ClientDisconnected
-      logger.debug("Client disconnected")
-    ensure
-      sse.close
     end
 
-    render nothing: true
+    head :ok
   end
 
   def simul
   end
 
   private
+  def perform_task(stream)
+    sse = ActionController::Live::SSE.new(stream, retry: 300, event: "message")
+
+    # long polling
+    loop do
+      data = @tournament.get_results(@round)
+      unless data.empty?
+        sse.write(JSON.pretty_generate(data))
+      end
+      sleep 3
+    end
+  rescue Errno::EPIPE
+    logger.debug("Client disconnected")
+  ensure
+    sse.close
+  end
+
   def set_tournament
     @tournament = Tournament.find_by(fp: true)
   end

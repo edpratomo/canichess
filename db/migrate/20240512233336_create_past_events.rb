e686e2f188d2879a95ecc676f09c31122e4067d7
Edwin Pratomo <edpratomo@cpan.org>
2024-05-17 06:32:52 +0700

    fixed Home link

--- db/migrate/20240512233336_create_past_events.rb
+++ db/migrate/20240512233336_create_past_events.rb
@@ -4,6 +4,16 @@ class CreatePastEvents < ActiveRecord::Migration[6.1]
       t.references :eventable, polymorphic: true, index: true
       t.timestamps
     end
+
+    tourneys = Tournament.where("id > 1").order(id: :desc)
+    simul = Simul.first
+
+    tr1 = Tournament.find(2)
+    PastEvent.create(eventable: tr1)
+    sm1 = Simul.find(1)
+    PastEvent.create(eventable: sm1)
+    tr2 = Tournament.find(3)
+    PastEvent.create(eventable: tr2)
   end
 
   def down

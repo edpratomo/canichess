8cc2faa2491a0c5ddadd13260618d2b0c5c9b143
Edwin Pratomo <edpratomo@cpan.org>
2024-05-23 12:40:27 +0700

    updated home controller's index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,30 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-  before_action :set_tournament
+  before_action :set_front_page
 
   layout 'top-nav.html.erb'
-  
+
   def index
-    redirect_to tournament_path(@tournament)
+    if @front_page
+      if @front_page.is_a? Tournament
+        redirect_to tournament_path(@front_page)
+      elsif @front_page.is_a? Simul
+        redirect_to simul_path(@front_page)
+      end
+    end
   end
 
   def contact
   end
 
   private
+  def set_front_page
+    front_page = PastEvent.includes(:eventable).find {|e| e.eventable.fp }
+    if front_page
+      @front_page = front_page.eventable
+    end
+  end
+
   def set_tournament
     @tournament = Tournament.find_by(fp: true)
   end

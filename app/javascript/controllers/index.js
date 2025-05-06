b64c92ddf3428c2e5a7862c063bf7fb9de36a99d
Edwin Pratomo <edpratomo@cpan.org>
2025-05-07 01:59:24 +0700

    added players_filename_controller stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import PlayersFilenameController from "./players_filename_controller"
+application.register("players-filename", PlayersFilenameController)

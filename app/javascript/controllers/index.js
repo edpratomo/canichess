7927fc4e5979e689203eaaa557fc146105bd1a39
Edwin Pratomo <edpratomo@cpan.org>
2025-05-11 20:13:51 +0700

    added stimulus controller for escape button

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import EscapeController from "./escape_controller"
+application.register("escape", EscapeController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 

5ececf7ab8f00de7c73a4ddb1ecdc19b9e5f4307
Edwin Pratomo <edpratomo@cpan.org>
2025-05-29 06:05:30 +0700

    added production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server 'canichess', port: 22, roles: [:web, :app, :db], primary: true
 
 
 # role-based syntax

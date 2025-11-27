3ae5432c887e4410410a3db03d3675d3821feb09
Edwin Pratomo <edpratomo@cpan.org>
2025-11-28 00:14:31 +0700

    update boards table as well

--- db/migrate/20251127162919_copy_tournaments_attrs.rb
+++ db/migrate/20251127162919_copy_tournaments_attrs.rb
@@ -1,5 +1,9 @@
 class CopyTournamentsAttrs < ActiveRecord::Migration[6.1]
   def change
+    execute <<-SQL
+DROP TRIGGER IF EXISTS boards_if_modified ON boards;
+SQL
+
     tourneys = Tournament.where('id < 71')
     tourneys.each do |e|
       grp = e.groups.first
@@ -9,6 +13,14 @@ class CopyTournamentsAttrs < ActiveRecord::Migration[6.1]
       e.tournaments_players.each do |tp|
         tp.update!(group: grp)
       end
+
+      e.boards.each do |brd|
+        brd.update!(group: grp)
+      end
     end
+  
+    execute <<-SQL
+CREATE TRIGGER boards_if_modified AFTER DELETE OR UPDATE ON boards FOR EACH ROW EXECUTE PROCEDURE update_points_configurable();
+SQL
   end
 end

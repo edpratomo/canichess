0ef1970899c9f201f739a81631036102b08b7134
Edwin Pratomo <edpratomo@cpan.org>
2025-12-10 20:11:19 +0700

    added elo win probability formula

--- script/gen-random-scores.rb
+++ script/gen-random-scores.rb
@@ -12,6 +12,24 @@ def random_result
   end
 end
 
+def random_result2(white_rating, black_rating)
+  # Calculate win probability for white based on rating difference
+  rating_diff = white_rating - black_rating
+  white_win_prob = 1.0 / (1.0 + 10.0 ** (-rating_diff / 400.0))
+  
+  prng = Random.new
+  rand_value = prng.rand
+  
+  # Probability thresholds
+  if rand_value < white_win_prob * 0.8  # 80% of calculated probability for wins
+    "white"
+  elsif rand_value < white_win_prob * 0.8 + (1 - white_win_prob) * 0.8  # Black win
+    "black"
+  else
+    "draw"
+  end
+end
+
 group_id = ARGV.shift
 gr = if group_id
   Group.find(group_id)
@@ -27,5 +45,5 @@ exit if curr_round == 0
 
 boards = gr.boards.where(round: curr_round)
 boards.each do |board|
-  board.update!(result: random_result) if board.result.nil?
+  board.update!(result: random_result2(board.white.rating, board.black.rating)) if board.result.nil?
 end

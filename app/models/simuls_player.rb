a323074bbf599f420ced01936186f4413074fe1c
Edwin Pratomo <edpratomo@cpan.org>
2026-01-03 20:53:20 +0700

    added SimulsPlayer#update_number

--- app/models/simuls_player.rb
+++ app/models/simuls_player.rb
@@ -3,16 +3,38 @@ class SimulsPlayer < ApplicationRecord
   belongs_to :player
 
   after_commit :broadcast_score, on: :update
-  after_commit :broadcast_result, on: :update
+  after_commit :broadcast_result, on: :update, if: :result_changed?
 
-  after_commit  :regen_numbers, on: :destroy
+  after_commit :regen_numbers, on: :destroy
 
   def name
     player.name
   end
 
   def swap_number other_player
-    self.number, other_player.number = other_player.number, self.number
+    curr_number = self.number
+    transaction do
+      self.update(number: other_player.number)
+      other_player.update(number: curr_number)
+    end
+  end
+
+  def update_number inc_or_dec_number
+    transaction do
+      if inc_or_dec_number == "increase"
+        # find next player
+        next_player = SimulsPlayer.find_by(simul: simul, number: self.number + 1)
+        if next_player
+          self.swap_number(next_player)
+        end
+      else
+        # find prev player
+        prev_player = SimulsPlayer.find_by(simul: simul, number: self.number - 1)
+        if prev_player
+          self.swap_number(prev_player)
+        end
+      end
+    end
   end
 
   protected

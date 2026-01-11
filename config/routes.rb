86b516cc2aa5efde4c35afc072f986dc381c2035
Edwin Pratomo <edpratomo@cpan.org>
2026-01-12 03:06:52 +0700

    added UserController for show, index

--- config/routes.rb
+++ config/routes.rb
@@ -124,7 +124,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users #, skip: [:registrations]
-  resources :users
+  resources :users, only: [:index, :show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"

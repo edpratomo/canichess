8182057c2a8aef4b05f2206951a5cbe54e39dfbb
Edwin Pratomo <edpratomo@cpan.org>
2026-01-12 10:47:09 +0700

    added users_controller#create

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,27 @@ class Admin::UsersController < ApplicationController
   def index
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "User created successfully"
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :fullname, :email, :password, :password_confirmation)
   end
 end

cc79c613a4d745b7e30ec6df8e18a06a4ba126cc
Edwin Pratomo <edpratomo@cpan.org>
2026-01-28 21:29:57 +0700

    fix for Logger NameError

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+# Fix for Logger NameError after concurrent-ruby update
+# must be required before "bundler/setup"
 require "logger"
+
+require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.

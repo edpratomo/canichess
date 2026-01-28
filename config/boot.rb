29b916e36db287f36b71cf19e91ec85ac3dacc95
Edwin Pratomo <edpratomo@cpan.org>
2026-01-29 00:03:56 +0700

    Revert "Revert "downgraded concurrent-ruby""
    This reverts commit d18f34ec894f0994ee0ca1755c38b884798f3d08.

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-# Fix for Logger NameError after concurrent-ruby update
-# must be required before "bundler/setup"
-require "logger"
-
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.

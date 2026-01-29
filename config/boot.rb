1e5be4766ddc9243551ab2cec89d200327d986da
Edwin Pratomo <edpratomo@cpan.org>
2026-01-30 03:22:43 +0700

    improved sidekiq docker image

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+if ! ENV["RAILS_MASTER_KEY"] && ENV["RAILS_MASTER_KEY_FILE"]
+  ENV["RAILS_MASTER_KEY"] = File.read(ENV["RAILS_MASTER_KEY_FILE"]).strip
+end
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
